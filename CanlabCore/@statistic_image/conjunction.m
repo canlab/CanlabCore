930acf911d8f380742e0f135e2c87a91975acfcc
Yoni Ashar <yoniashar@gmail.com>
2015-09-09 16:59:11 -0600

    added conjunction functionality for statistic_image

---
+++ CanlabCore/@statistic_image/conjunction.m
@@ -0,0 +1,42 @@
+% returns the conjunction of two statistic_images.  considers positive and
+% negative activations separately.
+%
+% input:  two thresholded statistic images
+% output:  a statistic_image with all voxels suprathreshold (in the same direction) in both input
+% images.  Voxel values are set to 1 and -1, to indicate direction.
+%
+% Yoni Ashar, Sept. 2015
+function conj = conjunction(si1, si2)
+
+% split first si into pos/neg significant voxels
+si1_pos = si1;
+posinds = si1.dat > 0;
+si1_pos.sig(~posinds) = 0; % unset significance of neg voxels
+
+si1_neg = si1;
+neginds = si1.dat < 0;
+si1_neg.sig(~neginds) = 0; % unset significance of pos voxels
+
+% repeat for 2nd si
+si2_pos = si2;
+posinds = si2.dat > 0;
+si2_pos.sig(~posinds) = 0;
+
+si2_neg = si2;
+neginds = si2.dat < 0;
+si2_neg.sig(~neginds) = 0;
+
+% the pos conjuction
+pos_conj = si1_pos;
+pos_conj.dat( pos_conj.dat > 0) = 1; % set all pos values to 1
+pos_conj.sig = si1_pos.sig & si2_pos.sig;
+
+% the neg conjuction
+neg_conj = si1_neg;
+neg_conj.dat( neg_conj.dat < 0) = -1; % set all neg values to -1
+neg_conj.sig = si1_neg.sig & si2_neg.sig;
+
+conj = si1; % to load image space, etc.
+conj.dat = pos_conj.dat + neg_conj.dat;
+conj.sig = pos_conj.sig + neg_conj.sig;
+

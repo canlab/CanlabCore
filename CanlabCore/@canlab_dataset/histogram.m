0f28067ab102c6e2788c423b391acc94a03e2092
Daniel Lee <daniel.h.lee@colorado.edu>
2016-02-19 14:11:36 -0700

    canlab_dataset documentation update
    documentation header update

--- CanlabCore/@canlab_dataset/histogram.m
+++ CanlabCore/@canlab_dataset/histogram.m
@@ -7,17 +7,42 @@ function fig_han = histogram(D, varargin)
 % :Usage:
 % ::
 %
-%    histogram(D, 'Anxiety');
+%    fig_han = histogram(D, [optional inputs]);
 %
+% ..
+%     Author and copyright information:
 %
-% :Optional Input:
+%     Copyright (C) 2013 Tor Wager
 %
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% ..
+%
+% :Inputs:
+%
+%   **D:**
+%        canlab_dataset
+%
+% :Optional Inputs:
 %   **nofig': suppress creation of new figure
 %
 %
-% ..
-%    Copyright Tor Wager, 2013
-% ..
+% :Outputs:
+%
+%   **fig_han:**
+%        figure handle
+%
+
 
 fig_han = [];
 dofig = 1;

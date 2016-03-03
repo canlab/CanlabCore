192d8af26c3787f3671121da32105cc646b38936
Yoni Ashar <yoniashar@gmail.com>
2016-03-03 06:22:16 -0700

    improved doc and functionality of get_wh_image

--- CanlabCore/@image_vector/get_wh_image.m
+++ CanlabCore/@image_vector/get_wh_image.m
@@ -32,14 +32,27 @@ end
 
 out = dat;
 
+% for each field, if the field is of the same size as dat, select the 'wh'
+% column
 fnames = fieldnames(dat);
-
 for f=fnames'
     
     field = char(f);
-    if isequal( size(out.(field)) , size(dat.dat) ) % this is a field to be selected down
+    if isequal( size(out.(field)) , size(dat.dat) )
         
         out.(field) = out.(field)(:,wh);
         
     end
 end
+
+% and then grab a few more fields that are not found with the size check
+% above.  these field are all 1D
+
+otherfields = {'image_names', 'fullpath', 'removed_images'};
+for f = otherfields
+    field = char(f);
+    if ~isempty(out.(field))
+        out.(field) = out.(field)(wh); % these field are all 1D
+    end
+end
+        

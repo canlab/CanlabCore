974eec12fb122cbf3e643ec82aef7275ec9c37c2
torwager <torwager@gmail.com>
2017-04-11 13:32:28 -0600

    replace_values method

--- CanlabCore/@canlab_dataset/replace_values.m
+++ CanlabCore/@canlab_dataset/replace_values.m
@@ -1,8 +1,41 @@
-function D = exclude_values(D, varname, function_handle)
+function D = replace_values(D, varname, function_handle, replace_with)
 % Replace values identified with your custom input function with NaN
 %
-% e.g., 
+% D = replace_values(D, function_handle to select values to replace, replace_with, varname)
+%
+% function_handle is an anonymous function that selects values to replace.
+% It should return a logical vector of true/false values. e.g.,  @(x) x <= 0
+%
+% e.g.,
 % Take the variable 'ratings' in canlab_dataset object D
 % Replace all ratings <= 0 with NaN
+%
+% D = replace_values(D, 'ratings', @(x) x <= 0, NaN)
 
-D = omit_values(D, 'ratings', @(x) x <= 0)
+[dat, datcell, wh_level, descrip, wh_indx] = get_var(D, varname);  % to list variables
+
+switch wh_level
+    case 1  % Subject
+        
+        wh = function_handle(dat); % select observations to replace, logical
+        
+        dat(wh) = replace_with;
+        
+        D.Subj_Level.data(:, wh_indx) = dat;
+        
+    case 2  % Event
+        
+        n = length(datcell);  % for each subject
+        
+        for i = 1:n
+            
+            wh = function_handle(datcell{i}); % select observations to replace, logical
+            datcell{i}(wh) = replace_with;
+            
+            D.Event_Level.data{i}(:, wh_indx) = datcell{i};
+            
+        end % subject
+        
+end
+
+end % function

b662246c8fa74fae73f2bebee8fcc218b2415167
torwager <torwager@gmail.com>
2017-05-13 00:26:38 -0600

    minor

--- CanlabCore/Visualization_functions/colorcube_colors.m
+++ CanlabCore/Visualization_functions/colorcube_colors.m
@@ -2,11 +2,18 @@ function colors = colorcube_colors(varargin)
 
 wh = [10 11 13 17 18 21 22 23 28 30 32 24 36];
 
+nofigs = isempty(findobj(0, 'Type', 'Figure'));
+
 cm = colormap('colorcube');
 cm = [cm(wh, :); cm];
 cm = mat2cell(cm, ones(size(cm, 1)), 3)';
 
 colors = cm;
+
+if nofigs
+    % we created one for colormap, so close
+    close
+end
 
 if ~isempty(varargin)
     n = varargin{1};

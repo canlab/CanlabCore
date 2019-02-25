a2d138686b8c28214c8e4745c7404218338e01c9
bogpetre <bogpetre@gmail.com>
2019-02-25 15:45:28 -0700

    Updated table2canlab_dataset to handle textdata

--- CanlabCore/Data_processing_tools/table2canlab_dataset.m
+++ CanlabCore/Data_processing_tools/table2canlab_dataset.m
@@ -28,7 +28,33 @@ DAT.Subj_Level.names = StudyTable.Properties.VariableNames;
 % Data from table
 % -------------------------------------------------------------------------
 
-DAT.Subj_Level.data = table2array(StudyTable);
+studyCell = table2cell(StudyTable);
+studyRow = studyCell(1,:);
+numeric = ones(size(studyRow));
+character = ones(size(studyRow));
+for i = 1:length(studyRow)
+    if ~isnumeric(studyRow{i});
+        numeric(i) = 0;
+    end
+    if ~ischar(studyRow{i})
+        character(i) = 0;
+    end
+end
+
+if any(numeric==0) % upload numeric data to rows where available, nans elsewhere, upload text to textdata.
+    warning(sprintf('Table is a cell array. Normally entire table gets assigned to Subj_Level.data. Will additionally assign text data to Subj_Level.textdata. Unexpected things may happen downstream.'));
+    if sum(numeric) + sum(character) ~= size(studyRow,2)
+        error('Non numeric and non character data is no supported by table2canlab_dataset. Please remove nontext and nonnumeric classes from table and try again.');
+    end
+    DAT.Subj_Level.data = zeros(size(studyCell));
+    DAT.Subj_Level.data(:) = nan;
+    DAT.Subj_Level.data(:,logical(numeric)) =  cell2mat(studyCell(:,logical(numeric)));
+    
+    DAT.Subj_Level.textdata = cell(size(studyCell));
+    DAT.Subj_Level.textdata(:,logical(character)) = studyCell(:,logical(character));
+else
+    DAT.Subj_Level.data = table2array(StudyTable);
+end
 DAT.Subj_Level.descrip{1} = 'Imported from Table object. Add descriptions.';
 DAT.Subj_Level.type = repmat({'numeric'}, 1, length(DAT.Subj_Level.names));
 

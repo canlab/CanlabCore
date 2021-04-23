94ef830a81980ec34fb5c23c2b3e4825f1f0d07d
Tor Wager <tor.wager@colorado.edu>
2021-04-23 14:04:43 -0400

    Create subdivide_by_atlas.m

---
+++ CanlabCore/@image_vector/subdivide_by_atlas.m
@@ -0,0 +1,6 @@
+% 1. Take image_vector object with thresholded results (e.g., activation map; fmri_data or statistic_image)
+% 2. Convert to region( )
+% 3. subdivide_by_atlas( )
+% 4. convert region to atlas object
+% 5. [optional]: table.
+

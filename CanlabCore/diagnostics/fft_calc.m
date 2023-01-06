9536fec06515da92e8123123947685377111330a
Tor Wager <torwager@gmail.com>
2023-01-06 17:20:08 -0500

    Update fft_calc.m

--- CanlabCore/diagnostics/fft_calc.m
+++ CanlabCore/diagnostics/fft_calc.m
@@ -1,4 +1,4 @@
-function [myfft, freq] = fft_calc(dat, TR)
+function [myfft, freq, h, h2] = fft_calc(dat, TR, varargin)
 % Simple function to calculate the FFT power of a 
 % data vector (dat) as a function of frequency,
 % given a sample-to-sample repetition time (TR)
@@ -6,17 +6,65 @@ function [myfft, freq] = fft_calc(dat, TR)
 % :Usage:
 % ::
 %
-%     [myfft, freq] = fft_calc(dat, TR)
+%     [myfft, freq, line_handle, nyquist_line_handle] = fft_calc(dat, TR)
 %
+% Example:
+% Create and plot a sin wave:
+% ------------------------------------------------------------------
+% Fs = 1000;            % Sampling frequency, e.g., Hz (samples/sec)                    
+% T = 1/Fs;             % Sampling period (time between samples, e.g., sec)
+% L = 1000;             % Length of signal (in samples)
+% t = (0:L-1)*T;        % Time vector
+% 
+% % Sine wave parameters
+% theta = 0;            % Phase, in radians
+% F = 10;               % Frequency of sin wave (cycles/sec)
+% 
+% y = sin(2 * pi * F * t + theta);
+% 
+% create_figure('sin wave');
+% plot(t, y)
+% xlabel('Time (sec)')
+%
+% Downsample the sin wave at new frequency Fs and plot signal and FFT:
+% ------------------------------------------------------------------
+% Fs = 20;  % New sampling frequency
+% downsample_by = round(1000/Fs);
+% 
+% figure;
+% plot(t, y, 'k-'); hold on
+% set(gcf, 'Position', [0 644        1358         231])
+% 
+% plot(t(1:downsample_by:end), y(1:downsample_by:end), '.-', 'Color', [1 .3 .6], 'LineWidth', 2);
+% Nyquist = Fs/2;
+% title(sprintf('Sampling rate = %3.1f, Nyquist limit = %3.2f', Fs, Nyquist))
+% 
+% figure; hold on;
+% [myfft, freq, line_han] = fft_calc(y, 1/1000);
+% set(line_han, 'Color', 'k', 'LineWidth', 2, 'LineStyle', '-');
+% 
+% [myfft, freq, line_han] = fft_calc(y(1:downsample_by:end), 1/Fs);
+% set(line_han, 'Color', [1 .3 .6], 'LineWidth', 2, 'LineStyle', '-');
+% 
+% set(gca, 'XLim', [0 (max(Fs, 1.2 * F))]);
+
+
 
 n = length(dat);
+
+if ~isempty(varargin)
+    if ischar(varargin{1}) && strcmp(varargin{1}, 'plot')
+    else
+        doplot = varargin{1};
+    end
+end
 
 % from matlab example
 % power = abs(Y(1:floor(n/2))).^2;
 % nyquist = 1/2;
 % freq = (1:n/2)/(n/2)*nyquist
 
-nyq = 1 ./ (2 * TR);
+nyq = 1 ./ (2 * TR);  % Sampling rate frequency / 2. TR = sample-to-sample time, 1/Fs
 
 timepts = floor(n ./ 2);
 
@@ -27,3 +75,15 @@ myfft = fft(dat); %real(abs(fft(dat)));
 myfft = abs(myfft(1:timepts)) .^ 2;  % power
 
 myfft = myfft ./ sum(myfft);
+
+h = plot(freq, myfft); 
+xlabel('Frequency (Hz)')
+title('Frequency domain')
+
+% nyquist = Fs ./ 2;  % Any signal faster than Nyquist freq will be aliased
+
+h2 = plot_vertical_line(nyq);
+set(h, 'LineStyle', ':')
+
+end
+

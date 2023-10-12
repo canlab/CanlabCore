a3b9cf2f0574dd56e614dc19d3458948658116e3
Bogdan Petre <bogpetre@gmail.com>
2023-10-11 23:12:21 -0500

    More graceful dropping of regions

--- CanlabCore/@atlas/probability_maps_to_region_index.m
+++ CanlabCore/@atlas/probability_maps_to_region_index.m
@@ -14,7 +14,20 @@ allempty = all(dat.probability_maps == 0, 2) | isnan(maxval);  % some out-of-mas
 condf(allempty) = 0;
 
 dat.dat = int32(condf);
-
+n_regions = size(dat.probability_maps,2);
+if length(unique(condf)) < n_regions
+    dropped_ind = find(~ismember(1:n_regions,unique(condf)));
+    labels = cell(length(dropped_ind),1);
+    fnames = {'labels_2','labels_3','labels_4','labels_5','label_descriptions','labels'};
+    for f = 1:length(fnames)
+        if length(dat.(fnames{f})) == n_regions
+            labels = dat.(fnames{f});
+            dat.(fnames{f})(dropped_ind) = [];
+        end
+    end
+    for i = 1:length(dropped_ind)
+        warning('Dropping region %d: %s', dropped_ind(i), labels{dropped_ind(i)});
+    end
 end
 
 

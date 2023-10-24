ceade8a12a2d56301f6ea32ab7de5dcf32a98a9e
Zizhuang-Miao <112502588+Zizhuang-Miao@users.noreply.github.com>
2023-10-24 17:41:01 -0400

    Update conjunction.m
    allow for generating a [1 -1] map (as designed before)

--- CanlabCore/@statistic_image/conjunction.m
+++ CanlabCore/@statistic_image/conjunction.m
@@ -1,4 +1,4 @@
-function conj = conjunction(si1, si2, varargin)
+function conj = conjunction(si1, si2, type, varargin)
 % Returns the conjunction of two statistic_images.  considers positive and
 % negative activations separately.
 %
@@ -15,13 +15,31 @@ function conj = conjunction(si1, si2, varargin)
 % ..
 %    Yoni Ashar, Sept. 2015
 % ..
+% 
+% ..
+%    Zizhuang Miao, Oct. 2023
+% .. 
+% 
+% :Inputs:
+%
+%   Added another input argument "type", allowed values are "indicator" or
+% "values". "indicator" will return an output with voxel values set to 1 
+% and -1 (as previously designed); "values" will return the average voxel
+% values of the two input maps. Default to "values" to prevent crashing
+% previous scripts using this function.
+% 
 
 direction = 0; % default: both pos and neg
-if nargin > 2
+if nargin == 2
+    type = "values"; % default: average of two maps
+end
+if nargin > 3
     direction = varargin{1};
 end
 
 if compare_space(si1, si2), error('Objects are not in same space'); end
+
+if ~ismember(type, {'indicator', 'values'}), error('The input type is not allowed'); end
 
 % split first si into pos/neg significant voxels
 si1_pos = si1;
@@ -43,24 +61,39 @@ si2_neg.sig(~neginds) = 0;
 
 % the pos conjuction
 pos_conj = si1_pos;
-pos_conj.dat( pos_conj.dat > 0) = 1; % set all pos values to 1
+pos_conj.dat(pos_conj.dat > 0) = 1; % set all pos values to 1
+pos_conj.dat(pos_conj.dat < 0) = 0; % set all neg values to 0
 pos_conj.sig = si1_pos.sig & si2_pos.sig;
 
 % the neg conjuction
 neg_conj = si1_neg;
-neg_conj.dat( neg_conj.dat < 0) = -1; % set all neg values to -1
+neg_conj.dat(neg_conj.dat < 0) = -1; % set all neg values to -1
+neg_conj.dat(neg_conj.dat > 0) = 0; % set all pos values to 0
 neg_conj.sig = si1_neg.sig & si2_neg.sig;
 
-if direction==0 % both pos and neg
-    conj = si1; % to load image space, etc.
-    conj.dat = pos_conj.dat + neg_conj.dat;
-    conj.sig = pos_conj.sig + neg_conj.sig;
-
-elseif direction < 0 % only neg
-    conj = neg_conj;
-
-else % only pos
-    conj = pos_conj;
+if strcmp(type, 'indicator')    % output a [1 -1] map
+    if direction==0 % both pos and neg
+        conj = si1; % to load image space, etc.
+        conj.dat = pos_conj.dat + neg_conj.dat;
+        conj.sig = pos_conj.sig + neg_conj.sig;
+    
+    elseif direction < 0 % only neg
+        conj = neg_conj;
+    
+    else % only pos
+        conj = pos_conj;
+    end
+elseif strcmp(type, 'values')    % output a value map
+    conj = si1; 
+    conj.dat = (si1.dat + si2.dat)/2;    % averaging the two maps
+    if direction==0 
+        conj.sig = pos_conj.sig + neg_conj.sig;
+    
+    elseif direction < 0 % only neg
+        conj.sig = neg_conj.sig;
+    
+    else % only pos
+        conj.sig = pos_conj.sig;
+    end
 end
-        
     

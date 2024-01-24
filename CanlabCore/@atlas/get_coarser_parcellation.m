6432ab0b6a6786a8b8929a4e6a198f4acceadd44
Bogdan Petre <f0042vm@discovery7.hpcc.dartmouth.edu>
2024-01-24 01:02:42 -0500

    Bug fixes for label field shifts

--- CanlabCore/@atlas/get_coarser_parcellation.m
+++ CanlabCore/@atlas/get_coarser_parcellation.m
@@ -40,40 +40,26 @@ end
 new_lbl = valid_lbls{new_ind};
 % note: we remove any label fields below if they're not of the appropriate
 % length to correspond to the original parcellation
-kept_lbl_ind = find((n_unique_lbls < n_unique_lbls(new_ind)) & ...
+kept_lbl_ind = find((n_unique_lbls <= n_unique_lbls(new_ind)) & ...
     (n_lbls == length(obj.(labelfield))));
-rm_lbl_ind = find((n_unique_lbls > n_unique_lbls(new_ind)) | ...
-    (n_lbls ~= length(obj.(labelfield))) | ...
-    strcmp(labelfield,valid_lbls));
 
-% leave 'labels' unchanged. merge_atlases call will handle it
-kept_lbl_ind = kept_lbl_ind(~ismember(kept_lbl_ind, 1));
-rm_lbl_ind = rm_lbl_ind(~ismember(rm_lbl_ind, 1));
+rm_lbl_ind = find(~ismember(1:length(valid_lbls), 1:length(kept_lbl_ind)));
 
 [new_lbl_parcels, lbl_exp] = unique(obj.(new_lbl),'stable');
 new_parcels = cell(1,length(new_lbl_parcels));
-new_labels = cell(length(valid_lbls), length(new_lbl_parcels));
-for i = 1:length(new_lbl_parcels)
-    this_parcel{i} = obj.select_atlas_subset(new_lbl_parcels(i), new_lbl, 'exact', 'flatten');
-    for j = 1:length(kept_lbl_ind)
-        new_labels{j,i} = obj.(valid_lbls{j}){i};
-    end
+parfor i = 1:length(new_lbl_parcels)
+    new_parcel{i} = obj.select_atlas_subset(new_lbl_parcels(i), new_lbl, 'exact', 'flatten');
 end
 
-new_atlas_obj = this_parcel{1};
-for i = 2:length(this_parcel)
-    new_atlas_obj = new_atlas_obj.merge_atlases(this_parcel{i});
+new_atlas_obj = new_parcel{1};
+for i = 2:length(new_parcel)
+    new_atlas_obj = new_atlas_obj.merge_atlases(new_parcel{i});
 end
 for i = 1:length(kept_lbl_ind)
-    % consider all labels coarser than new_lbl
+    % increment by 1 
     old_lbls = obj.(valid_lbls{kept_lbl_ind(i)});
 
-    new_atlas_obj.(valid_lbls{kept_lbl_ind(i)}) = cell(1,length(lbl_exp));
-    for j = 1:length(lbl_exp)
-        % Get the label in the equivalent index as the current ROI's index
-        new_coarse_lbl = old_lbls{lbl_exp(j)};
-        new_atlas_obj.(valid_lbls{kept_lbl_ind(i)}){j} = new_coarse_lbl;
-    end
+    new_atlas_obj.(valid_lbls{i}) = old_lbls(lbl_exp(1:length(lbl_exp)));
 end
 
 % delete finer parcellations

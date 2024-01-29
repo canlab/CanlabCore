86dbc53f46dfdeba7e23703953aa8626cac180eb
Bogdan Petre <f0042vm@discovery7.hpcc.dartmouth.edu>
2024-01-29 14:55:05 -0500

    fixed fsl6 label

--- CanlabCore/Data_extraction/load_atlas.m
+++ CanlabCore/Data_extraction/load_atlas.m
@@ -282,7 +282,7 @@ switch lower(atlas_file_name_or_keyword)
         create_atlas = @(x1)create_CANLab2023_atlas('MNI152NLin2009cAsym','fine',2);
 
     case {'canlab2023_fine_fsl6_2mm', 'canlab2023_fine_fsl6'}
-        savefile='CANLab2023_fsl6_fine_2mm_atlas_object.mat';
+        savefile='CANLab2023_MNI152NLin6Asym_fine_2mm_atlas_object.mat';
         varname = 'atlas_obj';
         docreate = true;
         create_atlas = @(x1)create_CANLab2023_atlas('MNI152NLin6Asym','fine',2);

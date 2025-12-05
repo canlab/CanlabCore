9aac327e56dabfd3b9e81b782766bd2f6e608188
Michael-Sun <msun@dartmouth.edu>
2025-12-05 13:42:31 -0500

    Some critical fixes to regress and intercept detection/creation if its missing in the design matrix.

--- CanlabCore/Statistics_tools/intercept.m
+++ CanlabCore/Statistics_tools/intercept.m
@@ -21,29 +21,45 @@ function X = intercept(X, meth)
 %
 %    X = intercept(X, 'end');
 %
+%   General usability edits and edge-case fixes by Michael Sun, Ph.D. on
+%   12/05/2025
 
-wh_is_intercept = find( ~any(diff(X)) ); % which column is the intercept?
+if nargin < 2
+    meth = 'which';
+end
 
-switch meth
+tol = 1e-8;  % tolerance for floating point
+
+% --- Find intercept columns: columns that are (approximately) all ones ---
+[nObs, nCols] = size(X);
+
+if nCols == 0 || nObs == 0
+    % Empty design matrix or no observations
+    wh_is_intercept = [];
+else
+    % A column is an intercept if all entries are ~1
+    is_intercept_col = all(abs(X - 1) < tol, 1);
+    wh_is_intercept  = find(is_intercept_col);
+end
+
+switch lower(meth)
     
     case 'which'
         X = wh_is_intercept;
         
     case 'remove'
-        
-        if ~isempty( wh_is_intercept ), X(:, wh_is_intercept) = []; end
+        if ~isempty(wh_is_intercept)
+            X(:, wh_is_intercept) = [];
+        end
         
     case 'add'
-        
         X(:, end+1) = 1;
         
     case 'end'
-        
         X = intercept(X, 'remove');
         X = intercept(X, 'add');
         
     otherwise
-        
         error('Unknown method');
         
 end
